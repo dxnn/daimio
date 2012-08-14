23f4de6019a2d82152d0dab6a3f76b12a1171f5d
dann toliver <dann@bentobox.net>
2012-08-14 02:27:20 -0400

    adding get.php. big /sigh. will remove soon.

---
+++ get.php
@@ -0,0 +1,31 @@
+<?php
+
+header("Content-type: application/javascript; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+
+$dir = $_GET['file'];
+
+if(!is_dir($dir)) {
+  if($file_contents = file_get_contents($dir)) {
+    echo $file_contents;
+  } else {
+    echo 'dir error'; 
+  }
+  
+  die;
+}
+
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir), RecursiveIteratorIterator::CHILD_FIRST);
+                                              
+foreach ($iterator as $path) {
+  if(!$path->isDir()) {
+    if(substr($path->__toString(), -2) == 'js') {
+      echo "//";
+      echo $path->__toString();
+      echo "\n";
+      readfile($path->__toString());
+    }
+  }
+}
+
+// EOF
