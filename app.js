c00a792a4742f282585cca39d9d945e7eb103c6a
dann toliver <dann@bentobox.net>
2013-01-17 12:05:51 -0500

    slideshow is working!

--- app.js
+++ app.js
@@ -1,43 +1,27 @@
-var http = require('http')
+var app = require('http').createServer(handler)
   , fs = require('fs')
   , static = require('node-static')
   , qs = require('querystring')
+  , io = require('socket.io').listen(app)
+  
+io.set('log level', 2)
     
-var DAML = require('daml');
-
-var fileServer = new(static.Server)('./public');
-
-var html = fs.readFileSync(__dirname+'/public/index.html.js', 'utf8');
-
-DAML.db = db;
-
+var DAML = require('daml')
+var fileServer = new(static.Server)('./public')
+var html = fs.readFileSync(__dirname+'/public/index.html.js', 'utf8')
 
 var onerror = function(err) {
-  return console.log(err);
+  return console.log(err)
 };
 
-var addNode = function(node) {
-  db.collection('nodes', function(err, c) {
-    c.count(function(err, count) {
-      console.log("There are " + count + " records in the nodes collection.");
-    });
-    
-    c.insert(node); // sync-style is ok here, because we're not waiting for confirmation
-  
-    c.find().toArray(function(err, items) {
-      console.log('there are ' + items.length + 'records, yep');
-    });
-  });
-};
-
-
-// Configure our HTTP server
-var app = http.createServer(function (req, res) {
+// Configure our HTTP server 
+function handler (req, res) {
   if(/public\//.test(req.url)) { // public files
     fileServer.serve(req, res);
     res.end();
     return;
   }
+  
   if(req.url === '/favicon.ico') { // favicon
     res.writeHead(200, {'Content-Type': 'image/x-icon'} );
     res.end();
@@ -49,9 +33,14 @@ var app = http.createServer(function (req, res) {
     req.on('data', function (data) {
       body += data;
       if(body.length > 1e6) req.connection.destroy();
-    });
+    })
+    
     req.on('end', function () {
       var POST = qs.parse(body); // no multipart forms // POST["node[name]"]
+
+      // HEY!! just bounce only bounce check header for ME
+      // then do like the DMG thing
+
 
       global.output = [];
       
@@ -61,203 +50,39 @@ var app = http.createServer(function (req, res) {
       // } 
       DAML.add_global('POST', POST);
       DAML.run(POST.daml);
-
-      setTimeout(function() {
-        res.writeHead(200, {"Content-Type": "application/json"});
-        res.end(JSON.stringify(global.output));
-      }, 300); // FIXME!!!
+      res.writeHead(200, {"Content-Type": "application/json"});
+      res.end(JSON.stringify(global.output));
     });
     return;
   }
   
   res.writeHead(200, {"Content-Type": "text/html"});
   res.end(html);
-});
+}
 
 
-db.open(function(err, db) {
-  if(err) return onerror('DB refused to open');
+console.log('connected!');
+app.listen(8008);
 
-  console.log('connected!');
+var counter = 0
+  , last_data = ''
+  , password = 'kjh1234kljh1324uiyhiuhfs98dfosdfhk2j3hk2jhsdfya9sd8fyasdfjh2k3jh234239uvnm23'
   
-  app.listen(8000);
-});
-
-
-    // // graceful shutdown
-    // process.once('SIGUSR2', function () {
-    //   gracefulShutdown(function () {
-    //     process.kill(process.pid, 'SIGUSR2'); 
-    //   })
-    // });
-
-
-/*
-
-  so... the next thing to do is to get daml working in node. right? that seems to make sense. get it working here on the server.
-  we'll need a way to catch it. maybe a /catch page for now. that seems nice and simple.
-  so we'll route /catch to the DAML parser, and send back whatever it gives us. nice and simple.
-  and then... we'll have to think about routing and content and 'pages'.
-  but first, the parser catcher.
-  and maybe some fancy chat-like things before rooms.
-  
-  OK! now we're gassing with cooks.
-  - add DAML to the clientside
-  - add {network send {"data find_noun"}} to the clientside
-  - 
-
-*/
-
-
-
-
-// var qs = require('querystring');
-// 
-// function (request, response) {
-//     if(request.method == 'POST') {
-//         var body = '';
-//         request.on('data', function (data) {
-//             body += data;
-//             if(body.length > 1e6) {
-//                 // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-//                 request.connection.destroy();
-//             }
-//         });
-//         request.on('end', function () {
-// 
-//             var POST = qs.parse(body);
-//             // use POST
-// 
-//         });
-//     }
-// }
-
-// var static = require('node-static');
-// 
-// //
-// // Create a node-static server instance to serve the './public' folder
-// //
-// var file = new(static.Server)('./public');
-// 
-// require('http').createServer(function (request, response) {
-//   request.addListener('end', function () {
-//     //
-//     // Serve files!
-//     //
-//     file.serve(request, response);
-//   });
-// }).listen(8080);
-
-
-
-
-// var findit = require('findit');
-// 
-// findit.find('/dir/to/walk', function (file) {
-//   //
-//   // This function is called each time a file is enumerated in the dir tree
-//   //
-//   console.log(file);
-// });
-
-// 
-// var app = require('http').createServer(function(req, res) {
-//   var html;
-//   
-//   if(req.url === '/favicon.ico') {
-//     res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-//     res.end();
-//     console.log('favicon requested');
-//     return;
-//   }
-//   console.log('hello');
-//   
-//   
-//   mongoid = req.url.slice(1);
-//   res.writeHead(200, {'Content-Type': 'text/html'});
-//   
-//   if(mongoid.length == 24) {
-//     html = require('fs').readFileSync(__dirname+'/index.html.js', 'utf8');
-//     html = html.replace(/mongoid_goes_here/g, mongoid);
-//     res.end(html);
-//   } else if(mongoid) {
-//     html = require('fs').readFileSync(__dirname+'/index.html.js', 'utf8');
-//     html = html.replace(/json_filename_goes_here/g, mongoid);
-//     res.end(html);
-//   } else {
-//     res.end('sorry');
-//   }
-// });
-// 
-// app.listen(1338);
-
-
-
-
-// // var nowjs = require("now");
-// // var everyone = nowjs.initialize(app);
-// 
-// everyone.now.enqueueOnServer = function(command){
-//   nowjs.getGroup(this.now.room).now.enqueueOnClient(command);
-// };
-// 
-// everyone.now.connectToGameOnServer = function() {
-//   var self = this, this_user, current_users = nowjs.getGroup(this.now.room).users;
-//   
-//   nowjs.getGroup(this.now.room).getUsers(function (user_ids) {
-//     if(!user_ids.length) {
-//       self.now.captain = true;
-//     }
-//     
-//     console.log(user_ids, self.now.room, self.user.clientId);
-//     
-//     for (var i = 0; i < user_ids.length; i++) {
-//       if(user_ids[i] == self.user.clientId) {continue;}
-//       this_user = current_users[user_ids[i]].now;
-//       console.dir(this_user);
-//       nowjs.getGroup(self.now.room).now.addPlayerOnClient(self.now.id, self.now.thing_key);
-//       self.now.addPlayerOnClient(this_user.id, this_user.thing_key, this_user.x, this_user.y);
-//     }
-//   });
-//   
-//   console.log("Joined: " + this.now.name);
-//   // everyone.now.addPlayerOnClient(this.now.name);
-// 
-//   nowjs.getGroup(this.now.room).addUser(this.user.clientId);
-//   nowjs.getGroup(this.now.room).now.enqueueOnClient('chat say name ' + this.now.name + ' phrase Hi_everyone!_' + this.now.name + '_has_joined_the_fun!');
-// }
-// 
-// nowjs.on('connect', function(){
-//   this.now.room = "room" + mongoid;
-//   this.now.tryBootOnClient();
-// });
-// 
-// 
-// nowjs.on('disconnect', function(){
-//   console.log("Left: " + this.now.name);
-//   nowjs.getGroup(this.now.room).now.removePlayerOnClient(this.now.id);
-//   nowjs.getGroup(this.now.room).now.enqueueOnClient('chat say name ' + this.now.name + ' phrase Has_left_the_maze.');
-// });
-// 
-// everyone.now.changeRoom = function(newRoom){
-//   nowjs.getGroup(this.now.room).removeUser(this.user.clientId);
-//   nowjs.getGroup(newRoom).addUser(this.user.clientId);
-//   this.now.room = newRoom;
-//   console.log('asdf ' + newRoom);
-//   // this.now.receiveMessage("SERVER", "You're now in " + this.now.room);
-// }
-// 
-
-// 
-// var mongodb = require('mongodb');
-// var server = new mongodb.Server("127.0.0.1", 27017, {});
-// new mongodb.Db('noder', server, {}).open(function (error, client) {
-//   if(error) throw error;
-//   var collection = new mongodb.Collection(client, 'test');
-//   collection.insert({foo: 'bar'}, {safe:true}, function(err, objects) {
-//     if(err) console.warn(err.message);
-//     if(err && err.message.indexOf('E11000 ') !== -1) {
-//       // this _id was already inserted in the database
-//     }
-//   });
-// });
+io.on('connection', function (socket) {
+  socket.on('primeme', function (data) {
+    if(data.pwd != password) return false
+    socket.on('bounce', function (data) {
+      if(data.daml.indexOf('current_slide') > -1) {
+        last_data = data
+        // console.log(last_data, data.daml.indexOf('current_slide'))
+      }
+      io.sockets.emit('bounced', data)
+      console.log(['bouncing', data])
+    })
+    io.sockets.emit('primer', {prime: true})
+  })
+  if(last_data) {
+    io.sockets.emit('bounced', last_data)
+  }
+  console.log(++counter)
+})
--- public/index.html.js
+++ public/index.html.js
@@ -3,59 +3,7 @@
 <head> 
   <title>Audio Adventures</title>
   
-  <style type="text/css" media="screen">
-    body {
-      padding: 0;
-      margin: 0;
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      left: 0px; top: 0px;
-    }
-
-    #presentation {
-      position: absolute;
-      height: 100%;
-      width: 100%;
-      left: 0px;
-      top: 0px;
-      display: block;
-      overflow: hidden;
-      background: #778;
-    }
-    
-    #header {
-      color: #ccc;
-      font: 5em 'Yanone Kaffeesatz';
-      letter-spacing: 2px;
-      position: absolute;
-      top: 10%;
-      left: 0;
-      width: 100%;
-      text-align: center;
-    }
-    
-    #bullets {
-      color: #ccc;
-      font: 5em 'Yanone Kaffeesatz';
-      letter-spacing: 2px;
-      position: absolute;
-      top: 10%;
-      left: 0;
-      width: 100%;
-      text-align: center;
-    }
-    
-    #code {
-      color: #ccc;
-      font-size: 2em;
-      background: #556;
-      position: absolute;
-      top: 40%;
-      left: 40%;
-    }
-  </style>
-  
+  <link href='http://sherpa.local/~dann/audio/public/css/main.css' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz' rel='stylesheet' type='text/css'>
 </head>
 
@@ -72,8 +20,19 @@
     {daml alias string "audio set-param name" as :set}
     {daml alias string "audio play" as :play}
     {daml alias string "audio pause" as :pause}
+    {daml alias string "audio reset" as :reset}
     {daml alias string "audio connect" as :connect}
-    {daml alias string "audio connect to 0" as :out}
+    {daml alias string "audio connect to 0" as :output}
+    
+    {begin nextstep | daml import into :deck as :next}
+      {@current_slide | add 1 | > :@current_slide}
+      {network bounce daml {"{@value | > :@current_slide}" | string transform from :@value to @current_slide}}
+    {end nextstep}
+
+    {begin nextstep | daml import into :deck as :prev}
+      {@current_slide | subtract 1 | > :@current_slide}
+      {network bounce daml {"{@value | > :@current_slide}" | string transform from :@value to @current_slide}}
+    {end nextstep}
 
     {/osc 5 | > :lfo | gain 80 | osc | out | play}
     {/lfo | set :frequency to 1}
@@ -83,74 +42,356 @@
   </script>
     
   <script type="text/daml" id="postload">
+    {(
+
+      {* (
+        :header ""
+        :code ""
+      )}
+      
+      {* (
+        :header "http://sherpa.local:8008"
+      )}
+      
+      {* (
+        :header "A Basic Oscillator"
+        :code "
+var osc = context.createOscillator()
+osc.connect(output)
+osc.frequency.value = 440
+osc.type = osc.SINE
+osc.noteOn(0)
+        "
+      )}
+      
+      {* (
+        :header "Voltage Controlled Oscillator"
+        :code "
+var osc = context.createOscillator()
+osc.connect(output)
+osc.noteOn(0)
+
+var gain = context.createGain();
+gain.gain.value = 80.0;
+gain.connect(osc.frequency);
+
+var lfo = context.createOscillator()
+lfo.frequency.value = 5
+lfo.connect(gain)
+lfo.noteOn(0)"
+      )}
+
+      {* (
+        :header "A 30-Second Primer on Sound"
+        :bullets (
+          "brain"
+          "ear"
+          "air"
+          "speaker"
+          "hardware"
+          "software"
+        )
+      )}
+      {* (
+        :header "Now with Harmonics"
+        :code "
+var gain1600 = context.createGain();
+gain1600.gain.value = 0.5;
+
+var osc1600 = context.createOscillator()
+osc1600.frequency.value = 1600
+osc1600.connect(gain1600)
+osc1600.noteOn(0)
+
+var gain1400 = context.createGain();
+gain1400.gain.value = 0.5;
+gain1600.connect(gain1400)
+
+var osc1400 = context.createOscillator()
+osc1400.frequency.value = 1400
+osc1400.connect(gain1400)
+osc1400.noteOn(0)
+
+var gain1200 = context.createGain();
+gain1200.gain.value = 0.5;
+gain1400.connect(gain1200)
+
+var osc1200 = context.createOscillator()
+osc1200.frequency.value = 1200
+osc1200.connect(gain1200)
+osc1200.noteOn(0)
+
+var gain1000 = context.createGain();
+gain1000.gain.value = 0.5;
+gain1200.connect(gain1000)
+
+var osc1000 = context.createOscillator()
+osc1000.frequency.value = 1000
+osc1000.connect(gain1000)
+osc1000.noteOn(0)
+
+var gain800 = context.createGain();
+gain800.gain.value = 0.5;
+gain1000.connect(gain800)
+
+var osc800 = context.createOscillator()
+osc800.frequency.value = 800
+osc800.connect(gain800)
+osc800.noteOn(0)
+
+var gain600 = context.createGain();
+gain600.gain.value = 0.5;
+gain800.connect(gain600)
+
+var osc600 = context.createOscillator()
+osc600.frequency.value = 600
+osc600.connect(gain600)
+osc600.noteOn(0)
+
+var gain400 = context.createGain();
+gain400.gain.value = 0.5;
+gain600.connect(gain400)
+
+var osc400 = context.createOscillator()
+osc400.frequency.value = 400
+osc400.connect(gain400)
+osc400.noteOn(0)
+
+var gain200 = context.createGain();
+gain200.gain.value = 0.5;
+gain400.connect(gain200)
+
+var osc200 = context.createOscillator()
+osc200.frequency.value = 200
+osc200.connect(gain200)
+osc200.noteOn(0)
+
+var gain100 = context.createGain();
+gain100.gain.value = 0.5;
+gain400.connect(gain100)
+
+var osc100 = context.createOscillator()
+osc100.frequency.value = 100
+osc100.connect(gain100)
+osc100.noteOn(0)
+
+gain100.connect(output)
+"
+      )}
+      
+      {* (
+        :header "Voltage Controlled Oscillator, redux"
+        :dform :true
+        :code "osc 5 | gain 80 | osc 440 | output"
+      )}
+
+      {* (
+        :header "Now with Color!"
+        :code "
+add a processing graph
+and an oscillator"
+      )}
+
+      {* (
+        :header "A 30-second FFT Primer"
+        :bullets ("<img src="http://intmstat.com/blog/2012/12/fourier-series2.gif" />" "<img src="http://ars.els-cdn.com/content/image/1-s2.0-S0886335003005248-gr1.gif" />")
+      )}
+
+      {* (
+        :header "A Synth Example"
+        :code "
+Add key binding for daml code
+Show spectrum and osc
+maybe show graph of daml
+maybe allow graph editing
+"
+      )}
+
+      {* (
+        :header "Convolution and Impulse Response"
+        :bullets (
+          "Room mapping"
+          "Echo effects"
+          "Stored as audio file"
+        )
+      )}
+
+      {* (
+        :header "Positional Audio"
+        :bullets (
+          "Panner and listener"
+          "Beachball"
+          "Ping Pong"
+          "Pinball"
+        )
+      )}
+
+      {* (
+        :header "3D Mapping"
+        :bullets (
+          "Inaudible audio"
+          "Motion tracking"
+          "Slightly terrifying"
+        )
+      )}
+
+      {* (
+        :header "Sing to Code"
+        :bullets (
+          "Create processing network"
+          "Genetic algorithms / machine learning"
+          "Parallelizable"
+        )
+      )}
+
+      {* (
+        :header "Penultima"
+        :bullets (
+          "Voice recognition / synthesis"
+          "Visualization networks"
+          "Distributed orchestra"
+          "Live coding"
+          "So much more..."
+        )
+      )}
+
+      {* (
+        :header "A/V Club!"
+        :bullets (
+          "dann @ twitter"
+          "dann @ bentobox dot net"
+        )
+      )}
+
+    ) | > :@slides}
+
+    {begin slider | variable bind path :@current_slide}
+      {@slides.#{@current_slide}.header | > :@header}
+      {@slides.#{@current_slide}.bullets | > :@bullets}
+      {@slides.#{@current_slide}.dform | > :@dform}
+      {@slides.#{@current_slide}.code | > :@code}
+    {end slider}
     
+    {1 | > :@current_slide}    
   </script>
 
   <div id="presentation">
     
     <div id="header">
       <script type="text/daml" data-var="@header">
-        <button class="btn {:active | if {@date_button | eq :april} }" id="april">April</button>
-        <button class="btn {:active | if {@date_button | eq :july} }" id="july">July</button>
-        <button class="btn {:active | if {@date_button | eq :nov} }" id="nov">November</button>
+        {@header}
       </script>
     </div>
     
     <div id="bullets">
       <script type="text/daml" data-var="@bullets">
-        <button class="btn {:active | if {@date_button | eq :april} }" id="april">April</button>
-        <button class="btn {:active | if {@date_button | eq :july} }" id="july">July</button>
-        <button class="btn {:active | if {@date_button | eq :nov} }" id="nov">November</button>
+        <ul>
+          {begin list | each data @bullets}
+            <li>{value}</li>
+          {end list}
+        </ul>
+      </script>
+    </div>
+    
+    <div id="dform">
+      <script type="text/daml" data-var="@dform">
+        {begin check | if @dform}
+          <form action="" method="" id="some_daml">
+            <input type="text" name="daml" value="" id="daml" />
+            <textarea name="commands" style="display:none">
+              {begin verbatim | quote}
+                {daml}
+              {end verbatim}
+            </textarea>
+          </form>
+        {end check}
       </script>
     </div>
     
     <pre>
-      <code id="code">
-        <script type="text/daml" data-var="@code">
-  				<button class="btn {:active | if {@date_button | eq :april} }" id="april">April</button>
-  				<button class="btn {:active | if {@date_button | eq :july} }" id="july">July</button>
-  				<button class="btn {:active | if {@date_button | eq :nov} }" id="nov">November</button>
-  			</script>
-      </code>
+      <code id="code"><script type="text/daml" data-var="@code">{@code | quote}</script></code>
     </pre>
+    
   </div>
 
+  <script type="text/javascript" src="http://sherpa.local/~dann/katsu/nodely/__underscore.js"></script>
+  <script type="text/javascript" src="http://sherpa.local/~dann/katsu/nodely/__jquery.js"></script>
+  <script type="text/javascript" src="http://sherpa.local/~dann/audio/get.php?file=node_modules/daml"></script>
+  <script src="/socket.io/socket.io.js"></script>
+  
   <script type="text/javascript">
     $(document).ready(function() {
       // preload
-      DAML.run($('#preload').text());
+      DAML.run($('#preload').text())
 
       // compile & bind the text/daml templates
       $("script[type*=daml]").each(function() {
         // id it
-        var begin, end, block, id, el, pid = $(this).parent()[0].id;
-        id = this.id || pid || (DAML.onerror("Can't find a block id for the script tag") && "foo");
+        var begin, end, block, id, el, pid = $(this).parent()[0].id
+        id = this.id || pid || (DAML.onerror("Can't find a block id for the script tag") && "foo")
       
-        if(id == 'preload') return;
+        if(id == 'preload') return
       
         // block it
-        begin = '{begin ' + id + '}';
-        end = '{end ' + id + '}';
-        block = begin + this.innerHTML + end;
-        DAML.run(block);
+        begin = '{begin ' + id + '} '
+        end = ' {end ' + id + '}'
+        block = begin + this.innerHTML + end
+        DAML.run(block)
       
         // attach it
-        el = this.dataset.el || (id == pid) ? pid : false;
+        el = this.dataset.el || (id == pid) ? pid : false
         if(el) {
-          DAML.run('{dom set_template id :' + el + ' daml ' + id + '}');
+          DAML.run('{dom set_template id :' + el + ' daml ' + id + '}')
           if(this.dataset.var) {
-            DAML.run('{variable bind path :' + this.dataset.var + ' daml "{dom refresh id :' + el + '}"}');
+            DAML.run('{variable bind path :' + this.dataset.var + ' daml "{dom refresh id :' + el + '}"}')
           }
         }
-      });
+      })
       
-      DAML.run(DAML.VARS.postload);
-    });
+      var keynavon = true
+      $(document).delegate("input, textarea", "focus", function(e){ keynavon = false })
+      $(document).delegate("input, textarea", "blur", function(e){ keynavon = true })
+      $(document).keydown( function(e) {
+        if (!keynavon || !DAML.ETC.prime) return true
+        
+        var keyCode = e.keyCode || e.which, arrow = {left: 37, up: 38, right: 39, down: 40 }
+        
+        switch (keyCode) {
+          case arrow.left:
+            e.preventDefault()
+            DAML.run('{deck prev}')
+          break
+          case arrow.right:
+            e.preventDefault()
+            DAML.run('{deck next}')
+          break
+        }
+      })
+      
+      
+      socket = io.connect('http://sherpa.local:8008');
+      socket.on('connect', function () {
+        // console.log('connect ok!')
+      });
+    
+      socket.on('bounced', function (data) {
+        if(DAML.ETC.prime) return false
+        DAML.run(data.daml, data.context)
+      });
+
+      socket.on('primer', function (data) {
+        if(data.prime == true) {
+          DAML.ETC.prime = true
+        }
+      });
+
+      primeme = function(pwd) {
+        socket.emit('primeme', {pwd: pwd})
+      }
+      
+      DAML.run(DAML.VARS.postload)
+    })
   </script>
-  
-  <script type="text/javascript" src="http://sherpa.local/~dann/katsu/nodely/__underscore.js"></script>
-  <script type="text/javascript" src="http://sherpa.local/~dann/katsu/nodely/__jquery.js"></script>
-  <script type="text/javascript" src="http://sherpa.local/~dann/audio/get.php?file=node_modules/daml"></script>
   
 </body>
 </html>
