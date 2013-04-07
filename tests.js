1092539c8c1355443ee8664b0e69ffe29b422404
dann toliver <dann@bentobox.net>
2013-04-07 17:45:01 -0400

    missed daml.js on last commit

--- tests.js
+++ tests.js
@@ -28,15 +28,44 @@
 
 // string->AB tests
 
-var app = require('http')
-    
+
+// LE PRELUDE
+
 var DAML = require('daml')
 
+ERRORS = []
 
 s2ABt = string_to_ABs_test = function(string, result) {
   var AB = DAML.string_to_ABlock(string)
+  delete AB.id
+  
   if(JSON.stringify(AB) == JSON.stringify(result))
     return false
     
-  console.log({in: string, out: AB, expected: result})
+  ERRORS.push({in: string, out: AB, expected: result})
+}
+
+// TESTS GO HERE!!!!
+
+s2ABt('asdf', {body:['asdf']} )
+s2ABt('{asdf}', {body:[ {head:[ {Alias: "asdf"} ]} ]} )
+s2ABt('{math add}', {body:[ {head:[ {Handler:"math", Method:"add"} ]} ]} )
+
+
+
+// WRAP IT ALL UP WITH A BOW
+
+show_errors = function(error) {
+  for(key in error) {
+    console.log(key + ': ' + JSON.stringify(error[key]))
+  }
+  console.log("")
+}
+
+if(ERRORS.length) {
+  console.log("ERRORS!\n")
+  ERRORS.forEach(show_errors)
+}
+else {
+  console.log('you win!')
 }
