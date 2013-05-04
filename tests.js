65089ba4ea47f774a0e59498f84b79648143fe9a
dann toliver <dann@bentobox.net>
2013-05-04 16:27:49 -0400

    exploding everything. very borken. stupidly stupid from lack of sleeps.

--- tests.js
+++ tests.js
@@ -333,11 +333,15 @@ funtest('{begin block | map data (1 2 3) | string join on ","} asdf {end block}'
 
 funtest('{(1 2 3) | map block "{add __ to 4}"}', '[5,6,7]')
 
+funtest('{map data (1 2 3 4) block "{__ | add 4} is ok"}', '["5 is ok","6 is ok","7 is ok","8 is ok"]')
+
+funtest('{map data (1 2 3 4) block "ok is {__ | add 4}"}', '["ok is 5","ok is 6","ok is 7","ok is 8"]')
+
 funtest('{begin foo | map data (1 2 3 4)}{__ | add 4}{end foo}', '[5,6,7,8]')
 
 funtest('{begin foo | map data (1 2 3 4) | string join on " "}{__ | add 4}{__ | add 4}{end foo}', '59 610 711 812')
 
-funtest('{begin foo | map data (1 2 3 4) | string join on " "}{__ | add 4}x{__ | add 4}{end foo}', '59 610 711 812')
+funtest('{begin foo | map data (1 2 3 4) | string join on " "}{__ | add 4}x{__ | add 4}{end foo}', '"5x5 6x6 7x7 8x8')
 
 funtest('{begin foo | map data (1 2 3 4) | string join on "---"}answer: {__ | add 4}{end foo}', 'answer: 4---answer: 4---answer: 4---answer: 4')
 
