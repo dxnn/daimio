58283bab390dbefdff9088b363e9010b53aa92e2
dann toliver <dann@bentobox.net>
2013-06-17 02:51:44 -0400

    some small progress toward reasonable spaceseeds; a new test; some refactorings and renamings.

---
+++ tests/node_templates.js
@@ -0,0 +1,66 @@
+/*
+
+  Spacial template testing
+  
+  Todo:
+  - make a template
+  - make another
+  - turn them into spaces
+  
+*/
+
+
+// string->AB tests
+
+
+// LE PRELUDE
+
+var DAML = require('daml')
+
+ERRORS = []
+
+template = function(template) {
+  var foo = DAML.spaceseed_add(template)
+
+  console.log(foo, JSON.stringify(DAML.SPACESEEDS, null, 2))
+}
+
+dialect = function(dialect) {
+  return DAML.dialect_add(dialect)
+}
+
+// TESTS GO HERE!!!!
+
+var dialect = dialect({commands: {math: {add: true}}, aliases: {add: "math add value"}})
+
+template({ id: 'quux'
+         , dialect: dialect
+         , stations: [423294921]
+         , spaces: ['foo']
+         , ports: [ {space: 'quux', name: 'quux-in', flavour: 'space-in', typehint: 'string'}
+                  , {space: 'quux', name: 'quux-out', flavour: 'space-out', typehint: 'string'}
+                  , {space: 'quux', station: 1, name: '_in', flavour: 'station', typehint: 'string'}
+                  , {space: 'quux', station: 1, name: '_out', flavour: 'station', typehint: 'string'}
+                  , {space: 'quux', station: 1, name: '_error', flavour: 'station', typehint: 'string'}
+                  , {space: 'foo', name: 'foo-in', flavour: 'space-in', typehint: 'string'}
+                  , {space: 'foo', name: 'foo-out', flavour: 'space-out', typehint: 'string'}
+                  ]
+         , routes: [[1,3], [4,6], [7,2]]
+        })
+
+// WRAP IT ALL UP WITH A BOW
+
+show_errors = function(error) {
+  for(key in error) {
+    console.log(key + ': ' + JSON.stringify(error[key], null, 2))
+  }
+  console.log("")
+}
+
+if(ERRORS.length) {
+  console.log("ERRORS!\n")
+  ERRORS.forEach(show_errors)
+}
+else {
+  console.log('you win!')
+}
