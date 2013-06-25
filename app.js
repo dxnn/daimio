622a1242649f16f4f0fd46fff13766af10651a52
dann toliver <dann@bentobox.net>
2013-06-25 18:51:19 -0400

    sending code over the network: activated.

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ DAML.db = db
 DAML.mongo = mongo
 
 
-var fileServer = new(static.Server)('./public')
+// var fileServer = new(static.Server)('./public')
 // var html = fs.readFileSync(__dirname+'/public/index.html.js', 'utf8')
 
-var html = fs.readFileSync(__dirname+'/button.html', 'utf8')
+var html = fs.readFileSync(__dirname+'/demos/silly_turtle3.html', 'utf8')
 
 var onerror = function(err) {
   return console.log(err)
@@ -24,11 +24,11 @@ var onerror = function(err) {
 
 // Configure our HTTP server 
 function handler (req, res) {
-  if(/public\//.test(req.url)) { // public files
-    fileServer.serve(req, res);
-    res.end();
-    return;
-  }
+  // if(/public\//.test(req.url)) { // public files
+  //   fileServer.serve(req, res);
+  //   res.end();
+  //   return;
+  // }
   
   if(req.url === '/favicon.ico') { // favicon
     res.writeHead(200, {'Content-Type': 'image/x-icon'} );
@@ -36,37 +36,37 @@ function handler (req, res) {
     return;
   }
   
-  if(req.method == 'POST') {
-    var body = '';
-    req.on('data', function (data) {
-      body += data;
-      if(body.length > 1e6) req.connection.destroy();
-    })
-    
-    req.on('end', function () {
-      var POST = qs.parse(body); // no multipart forms // POST["node[name]"]
-
-      // HEY!! just bounce only bounce check header for ME
-      // then do like the DMG thing
-
-
-      global.output = [];
-      
-      // if(POST.daml) {
-        // this_html += DAML.run(POST.daml);
-        // TODO: allow text through here, not just json
-      // } 
-      
-      res.writeHead(200, {"Content-Type": "application/json"});
-
-      // DAML.add_global('POST', POST);
-      DAML.run(POST.daml, function(value) {
-        res.end(JSON.stringify(global.output));
-      });
-      
-    });
-    return;
-  }
+  // if(req.method == 'POST') {
+  //   var body = '';
+  //   req.on('data', function (data) {
+  //     body += data;
+  //     if(body.length > 1e6) req.connection.destroy();
+  //   })
+  //   
+  //   req.on('end', function () {
+  //     var POST = qs.parse(body); // no multipart forms // POST["node[name]"]
+  // 
+  //     // HEY!! just bounce only bounce check header for ME
+  //     // then do like the DMG thing
+  // 
+  // 
+  //     global.output = [];
+  //     
+  //     // if(POST.daml) {
+  //       // this_html += DAML.run(POST.daml);
+  //       // TODO: allow text through here, not just json
+  //     // } 
+  //     
+  //     res.writeHead(200, {"Content-Type": "application/json"});
+  // 
+  //     // DAML.add_global('POST', POST);
+  //     DAML.run(POST.daml, function(value) {
+  //       res.end(JSON.stringify(global.output));
+  //     });
+  //     
+  //   });
+  //   return;
+  // }
   
   res.writeHead(200, {"Content-Type": "text/html"});
   res.end(html);
@@ -89,21 +89,33 @@ db.open(function(err, db) {
   
 });
 
+var last_user_id = 0
+  // , password = 'kjh1234kljh1324uiyhiuhfs98dfosdfhk2j3hk2jhsdfya9sd8fyasdfjh2k3jh234239uvnm23'
 
 io.on('connection', function (socket) {
+    // 
+    // socket.on('process', function (data) {
+    //   if(!data.daml) 
+    //     return false
+    //   
+    //   DAML.run(data.daml, function(value) { // TODO: add 'context' for run
+    //     io.sockets.emit('return', value) // TODO: return just to asker [maybe use jDaimio for this?]
+    //   })
+    // })
   
-  socket.on('process', function (data) {
-    if(!data.daml) 
-      return false
-    
-    DAML.run(data.daml, function(value) { // TODO: add 'context' for run
-      io.sockets.emit('return', value) // TODO: return just to asker [maybe use jDaimio for this?]
-    })
-  })
-
+  last_user_id += 1
+  user_id = last_user_id
+  
   socket.on('bounce', function (data) {
+    data.user = user_id
     io.sockets.emit('bounced', data)
     // console.log(['bouncing', data])
   })
+  
+  socket.on('disconnect', function () {
+    io.sockets.emit('disconnected', {user: user_id});
+  });
+    
+  io.sockets.emit('connected', {user: user_id})
   
 })
