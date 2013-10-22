fce28b69f95cf3cc6671856ddd616e21e11f31de
dann toliver <dann@bentobox.net>
2013-10-22 17:20:08 -0400

    uses the port's extra param, if available

--- daimio/pflavs/from-js.js
+++ daimio/pflavs/from-js.js
@@ -3,10 +3,18 @@ D.import_port_flavour('from-js', {
   // TODO: this currently works with a space seed instead of an individual space -- should be per-space instead
   pairup: function(port) {
     var self = this
-      , eventname = port.space.seed.id + '-' + this.settings.thing
+      , eventname = port.space.seed.id + '-' + this.name
     
     var callback = function(ship) {
-      self.enter(ship.detail)
+      var value = 1
+      
+      if(self.settings.all.length > 2)
+        value = self.settings.thing
+      
+      if(ship.detail !== undefined)
+        value = ship.detail
+
+      self.enter(value)
     }
 
     document.addEventListener(eventname, callback)
