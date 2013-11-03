9d5ef107428a896ed2ae2e5ed2d531fa6e9d7568
dann toliver <dann@bentobox.net>
2013-11-02 21:13:50 -0400

    moved socket into D.Etc.socket

--- daimio/pflavs/socket-in.js
+++ daimio/pflavs/socket-in.js
@@ -3,7 +3,10 @@ D.import_port_flavour('socket-in', {
   outside_add: function() {
     var self = this
     
-    socket.on('bounced', function (ship) {
+    if(!D.Etc.socket)
+      return D.set_error('You must place a valid socket connection in D.Etc.socket')
+    
+    D.Etc.socket.on('bounced', function (ship) {
       if(!ship.user) return false
       self.enter(ship)
     })
--- daimio/pflavs/socket-out.js
+++ daimio/pflavs/socket-out.js
@@ -1,7 +1,9 @@
 D.import_port_flavour('socket-out', {
   dir: 'out',
   outside_exit: function(ship) {
-    if(socket)
-      socket.emit('bounce', ship)
+    if(!D.Etc.socket)
+      return D.set_error('You must place a valid socket connection in D.Etc.socket')
+
+    D.Etc.socket.emit('bounce', ship)
   }
 })
