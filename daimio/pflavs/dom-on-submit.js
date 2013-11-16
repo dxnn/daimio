93884e71534db1b5de6f8a0cfa695719e335ccd3
dann toliver <dann@bentobox.net>
2013-11-16 02:22:00 -0500

    checkbox finder

--- daimio/pflavs/dom-on-submit.js
+++ daimio/pflavs/dom-on-submit.js
@@ -9,7 +9,10 @@ D.import_port_flavour('dom-on-submit', {
         
       // TODO: buckle down and have this suck out all form values, not just the easy ones. yes, it's ugly. but do it for the kittens.
       for(var i=0, l=element.length; i < l; i++) {
-        ship[element[i].name] = element[i].value
+        if(element[i].type == 'checkbox')
+          ship[element[i].name] = element[i].checked ? element[i].value : false
+        else
+          ship[element[i].name] = element[i].value
       }
       self.enter(ship) 
     }
