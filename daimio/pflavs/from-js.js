7f369594cde718c082fef678169d36cd4944be9c
dann toliver <dann@bentobox.net>
2014-01-13 18:27:36 -0500

    code simplification

--- daimio/pflavs/from-js.js
+++ daimio/pflavs/from-js.js
@@ -1,15 +1,9 @@
 D.import_port_flavour('from-js', {
   dir: 'in',
-  pairup: function(port) {
-    var self = this
-    
-    this.default_value = 1
-
-    if(this.settings.all.length > 2)
-      this.default_value = this.settings.thing
-
-    this.pair = port
-    port.pair = this
+  outside_add: function(port) {
+    this.default_value = this.settings.all.length > 2
+                       ? this.settings.thing
+                       : 1
   },
   enter: function(ship, process) {
     var value = ship !== undefined ? ship : this.default_value
