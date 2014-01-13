08bd95b09e69c4614300b9db609d66f6801912d6
dann toliver <dann@bentobox.net>
2014-01-13 18:26:48 -0500

    new checkbox array notation (yucky but necessary)

--- daimio/pflavs/dom-on-submit.js
+++ daimio/pflavs/dom-on-submit.js
@@ -3,16 +3,25 @@ D.import_port_flavour('dom-on-submit', {
   outside_add: function() {
     var self = this
     
-    var callback = function(value, event) {
-      var ship = {}
-        , element = event.target
+    var callback = function(value, event) {                     // TODO: buckle down and have this suck out 
+      var ship = {}                                             //       all form values, not just the easy ones. 
+      var element = event.target                                //       yes, it's ugly. but do it for the kittens.
         
-      // TODO: buckle down and have this suck out all form values, not just the easy ones. yes, it's ugly. but do it for the kittens.
       for(var i=0, l=element.length; i < l; i++) {
-        if(element[i].type == 'checkbox')
-          ship[element[i].name] = element[i].checked ? element[i].value : false
-        else
+        if(element[i].type == 'checkbox') {
+          var name = element[i].name
+          if(name.slice(-2) == '[]') {                          // yes, this is totally gross, but we need a way
+            name = name.slice(0,-2)                             // to distinguish between single checkboxes and
+            ship[name] = ship[name] ? ship[name] : []           // lists of checkboxes, and this is a well-known
+            if(element[i].checked)                              // strategy. please someone solve this betterly!
+              ship[name].push(element[i].value)
+          } else {
+            ship[name] = element[i].checked ? element[i].value : false
+          }
+        }
+        else {
           ship[element[i].name] = element[i].value
+        }
       }
       self.enter(ship) 
     }
